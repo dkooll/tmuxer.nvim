5b3cf343f451be60f6ea21a73723f251bd16a764
dkooll <dennis.kool@cloudnation.nl>
2025-12-31 14:46:00 +0100

    cleanups

--- lua/tmuxer/init.lua
+++ lua/tmuxer/init.lua
@@ -293,12 +293,7 @@ local function create_session_finder(sessions)
             return string.format("   %d: %s", entry.window_index, entry.window_name)
           end
           if entry.window_count > 1 and not entry.expanded then
-            local suffix = string.format("/%d windows", entry.window_count)
-            return displayer {
-              entry.session_name,
-              { "/" .. entry.parent, "TmuxerParentDir" },
-              { suffix, "TmuxerParentDir" },
-            }
+            return string.format("%s/%s/%d windows", entry.session_name, entry.parent, entry.window_count)
           end
           return displayer {
             entry.session_name .. "/",
